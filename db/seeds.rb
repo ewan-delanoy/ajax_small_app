e9c6b65517a4860eff69a137c5a3ae1f6222cbb7
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-09 15:55:45 +0100

    create seeds for things

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Thing.create(essence:"life",substance:"death");
+Thing.create(essence:"black",substance:"white");
+Thing.create(essence:"one",substance:"two");
