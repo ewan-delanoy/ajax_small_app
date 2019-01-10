0f53497e8b43e6b15a7845620f5fb30ed25509c9
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-10 09:37:25 +0100

    create tasks controller and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
   resources :things
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
