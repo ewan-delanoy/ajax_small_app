e661da375788b7ea3c3820440963bd3c12ccad02
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-10 09:38:22 +0100

    add task routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
   resources :things
+  resources :tasks
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
